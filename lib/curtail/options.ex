ef13d1242fbb7b624f31243a6ed7eab9e827e859
Moxley Stratton <moxley.stratton@gmail.com>
2025-10-19 14:16:27 -0700

    Fix for Elixir 1.19

--- lib/curtail/options.ex
+++ lib/curtail/options.ex
@@ -4,17 +4,20 @@ defmodule Curtail.Options do
 
   defstruct length: 100,
             omission: "...",
-            word_boundary: @default_word_boundary,
+            word_boundary: nil,
             break_token: nil
 
   alias __MODULE__
+
+  def default_word_boundary, do: @default_word_boundary
 
   def new(opts\\ []) do
     configure(struct(Options, opts))
   end
 
-  defp configure(opts = %Options{ word_boundary: true}) do
+  defp configure(opts = %Options{ word_boundary: boundary}) when boundary in [true, nil] do
     %Options{opts | word_boundary: @default_word_boundary}
   end
+
   defp configure(opts), do: opts
 end
